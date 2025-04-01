1d78bfb7dfd5b99b163214ccd3e265ca8a497728
j <josevflores911@gmail.com>
2025-04-01 00:27:04 -0300

    solid dropdown single operation

--- modules/dropdown_tabelausuario/ler_municipios.php
+++ modules/dropdown_tabelausuario/ler_municipios.php
@@ -1,11 +1,11 @@
 <?php
     error_reporting(0);
     error_reporting(E_ALL);
-    include_once "../../classes/cls_tabelausuario.php";
+    include_once "../../classes/cls_dropdownusuarios.php";
 
     $nm_estado = $_POST["nm_estado"];
     
-    $object = new cls_tabelausuario();
+    $object = new cls_dropdownusuarios();
     $result = $object->getAllCountyByState( $nm_estado);
     echo json_encode($result);
 ?>
